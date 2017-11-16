d1f52e1cc8bed59d48e52d702f31b2a6519c822c
Your Name <youremail@domain.com>
2017-11-16 17:27:28 +0300

    generated migration for adding picture to assignments. also migrated the db, and permitted pdf, doc and docx uploads in the picture uploader whitelist

---
+++ db/migrate/20171116141514_add_picture_to_assignments.rb
@@ -0,0 +1,5 @@
+class AddPictureToAssignments < ActiveRecord::Migration[5.1]
+  def change
+    add_column :assignments, :picture, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002055059) do
+ActiveRecord::Schema.define(version: 20171116141514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171002055059) do
     t.datetime "due_date"
     t.string "extra_resources"
     t.integer "level_id"
+    t.string "picture"
   end
 
   create_table "exams", force: :cascade do |t|
