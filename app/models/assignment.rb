78ef791b13bba304169ed90af1d1d5dd7962214b
Your Name <youremail@domain.com>
2017-11-16 17:35:12 +0300

    mounted the uploader to the assignment model

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,7 +1,8 @@
 class Assignment < ApplicationRecord
   belongs_to :subject
   belongs_to :level
+  mount_uploader :picture, PictureUploader
   def name_with_state
-    "#{level}"
+    level.to_s
   end
 end
